7b40fa3af2f3574e5d6564badba249f89023ba02
g-broche <gregory.broche@yahoo.fr>
2025-06-12 10:10:16 +0200

    initial commit at spring boot initialization

---
+++ src/main/java/com/gbroche/courseorganizer/CourseorganizerApplication.java
@@ -0,0 +1,13 @@
+package com.gbroche.courseorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseorganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseorganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gbroche/courseorganizer/CourseorganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbroche.courseorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseorganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
