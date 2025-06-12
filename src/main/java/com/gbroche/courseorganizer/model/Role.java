d1f1ec88cfe292a781dcc3770274a850576268a7
g-broche <gregory.broche@yahoo.fr>
2025-06-12 14:07:35 +0200

    implemented role crud

---
+++ src/main/java/com/gbroche/courseorganizer/model/Role.java
@@ -0,0 +1,38 @@
+package com.gbroche.courseorganizer.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String label;
+
+    public Role() {
+
+    }
+
+    public Role(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
